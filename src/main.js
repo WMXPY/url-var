34193fbf717d71d1c148bde2bc49b84b0f337059
WMXPY <wm@wmpcxpy.com>
2017-08-04 18:01:15 -0500

    bug fix

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ let urlVar = {
         let re = {};
         for (let i in string) {
             let regexp = new RegExp('[?|&]' + string[i] + '=' + '([^&;]+?)(&|#|;|$)');
-            re[i] = decodeURIComponent((regexp.exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
+            re[string[i]] = decodeURIComponent((regexp.exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
             if (string.length == 1) return re[i];
         }
         return re;
@@ -12,7 +12,7 @@ let urlVar = {
         let re = {};
         for (let i in string) {
             let regexp = new RegExp('[?|&]' + string[i] + '=' + '([^&;]+?)(&|#|;|$)');
-            re[i] = decodeURIComponent((regexp.exec(url) || [, ""])[1].replace(/\+/g, '%20')) || null;
+            re[string[i]] = decodeURIComponent((regexp.exec(url) || [, ""])[1].replace(/\+/g, '%20')) || null;
             if (string.length == 1) return re[i];
         }
         return re;
