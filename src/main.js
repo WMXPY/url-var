1ec263cb3ebc4d6068c6338b85a00ece87539764
WMXPY <wm@wmpcxpy.com>
2017-08-04 17:36:33 -0500

    better version

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
 let urlVar = {
-    url: function (string) {
-        let regexp = new RegExp('[?|&]' + string + '=' + '([^&;]+?)(&|#|;|$)');
-        return decodeURIComponent((regexp.exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
+    url: function (...string) {
+        let re = {};
+        for (let i in string) {
+            let regexp = new RegExp('[?|&]' + string[i] + '=' + '([^&;]+?)(&|#|;|$)');
+            re[i] = decodeURIComponent((regexp.exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
+            if (string.length == 1) return re[i];
+        }
+        return re;
     },
-    str: function (url, string) {
-        let regexp = new RegExp('[?|&]' + string + '=' + '([^&;]+?)(&|#|;|$)');
-        return decodeURIComponent((regexp.exec(url) || [, ""])[1].replace(/\+/g, '%20')) || null;
+    str: function (url, ...string) {
+        let re = {};
+        for (let i in string) {
+            let regexp = new RegExp('[?|&]' + string[i] + '=' + '([^&;]+?)(&|#|;|$)');
+            re[i] = decodeURIComponent((regexp.exec(url) || [, ""])[1].replace(/\+/g, '%20')) || null;
+            if (string.length == 1) return re[i];
+        }
+        return re;
     }
 }
-module.export['default'] = urlVar;
+module.exports = urlVar;
---
+++ test/dumbtest.js
@@ -0,0 +1,3 @@
+let a = require('../src/main')
+
+console.log(a.str('ad?a=3&b=10', 'a', 'b'))
